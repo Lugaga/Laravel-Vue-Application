24e8bacae3787b11b92d2a015dfd626639241a14
lugaga <maureezgaga@gmail.com>
2020-01-07 15:53:37 +0300

    Add Display Users in Table and Modal Window for Creating new User in Laravel and Vue

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,17 +1,77 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Users Component</div>
 
-                    <div class="card-body">
-                        I'm an example component!
-                    </div>
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+                    <button class='btn btn-success' data-toggle="modal" data-target="#addNew">Add New <i class='fas fa-user-plus fa-fw'></i></button>
+                  </div>
                 </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Modify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                          <a href="#">
+                              <i class='fa fa-edit blue'></i>
+                          </a>
+                          /
+                          <a href="#">
+                              <i class='fa fa-trash red'></i>
+                          </a>
+
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addNewLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close<i class='fas fa-window-close fa-fw'></i></button>
+                <button type="button" class="btn btn-primary">Create<i class='fas fa-plus fa-fw'></i></button>
+            </div>
             </div>
         </div>
-    </div>
+        </div>
+
+                </div>
+
+
 </template>
 
 <script>
